75d671eb666cb4462998b454c7687bd75c2a2c0a
Sheila Wittman <szandora789@gmail.com>
2018-06-09 17:44:17 +0000

    automatically backed up by learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -4,6 +4,31 @@ def bonus
     it "uses the []= method to change the hero and heroine's status from alive to dead" do
       expect(bonus[:montague][:hero][:status]).to eq("dead")
       expect(bonus[:capulet][:heroine][:status]).to eq("dead")
+
+
+      epic_tragedy = {
+         :montague => {
+            :patriarch => {name: "Lord Montague", age: "53"},
+            :matriarch => {name: "Lady Montague", age: "54"},
+            :hero => {name: "Romeo", age: "15", status: "alive"},
+            :hero_friends => [
+               {name: "Benvolio", age: "17", attitude: "worried"},
+               {name: "Mercutio", age: "18", attitude: "hot-headed"}
+            ]
+         },
+         :capulet => {
+            :patriarch => {name: "Lord Capulet", age: "50"},
+            :matriarch => {name: "Lady Capulet", age: "51"},
+            :heroine => {name: "Juliet", age: "15", status: "alive"},
+            :heroine_friends => [
+                {name: "Steven", age: "30", attitude: "confused"},
+                {name: "Nurse", age: "44", attitude: "worried"}
+            ]
+         }
+      }
+
+      epic_tragedy [:montague][:hero][:status] = "dead"
+      epic_tragedy [:capulet][:heroine][:status] = "dead"
     end
   end
 
